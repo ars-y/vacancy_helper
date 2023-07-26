f8bc69fba64856895797a443019d2a8a996926da
ArslanYadov <arsyy90@gmail.com>
2023-07-26 22:19:40 +0300

    add name for db

--- settings/globconf.py
+++ settings/globconf.py
@@ -3,4 +3,6 @@ from pathlib import Path
 
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
-DB_DIR: Path = BASE_DIR / 'DataBase'
+DB_DIR: Path = BASE_DIR / 'database'
+
+DB_NAME: str = str(DB_DIR / 'db.vacancies')
