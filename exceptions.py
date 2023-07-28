0f07c19d08f749d9a8a9b31bed5ba7aca5ce4e8d
ArslanYadov <arsyy90@gmail.com>
2023-07-28 12:57:26 +0300

    add initial url argument for url exception; change exception message

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,12 @@
 class URLValueException(ValueError):
     """Exception for URl is not correct."""
-    
-    def __init__(self, *args: object) -> None:
+
+    def __init__(self, url: str, *args: object) -> None:
         super().__init__(*args)
-    
+        self._url = url
+
     def __str__(self) -> str:
         if not self.args:
-            return 'initial url isn\'t correct'
+            return 'Initial url \'%s\' is invalid' % self._url
 
         return super().__str__()
