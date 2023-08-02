016586150b8e706262527f7d78e0151ce1ffb222
ArslanYadov <arsyy90@gmail.com>
2023-08-02 12:22:28 +0300

    VIDStorage handle storage vacancies id responsibility from BaseVacancyCollector

--- bases.py
+++ bases.py
@@ -1,9 +1,7 @@
 import aiohttp
 import asyncio
-import datetime as dt
-import shelve
 
-import db
+from db import VIDStorage
 from exceptions import URLValueException
 from processors import TextProcessor
 
@@ -26,56 +24,9 @@ class BaseVacancyCollector:
         self._params = params
         self._filters = filters or set()
 
-        self._db_file: str | None = None
-        self._max_age: int | None = None
-        self._prepare_collector()
+        self._storage = VIDStorage()
 
-    def _prepare_collector(self) -> None:
-        """
-        Creating database for vacancies.
-        Set values for collector attribute.
-        """
-        if not self._db_is_exists():
-            self._create_db()
-
-        attribute_setters: tuple = (
-            '_set_db_filename',
-            '_set_max_age',
-        )
-
-        for attr_name in attribute_setters:
-            setter = getattr(self, attr_name)
-            setter()
-
-    def _set_db_filename(self) -> None:
-        """Set database file name."""
-        self._db_file = db.get_db_filename()
-
-    def _set_max_age(self) -> None:
-        """Set storage time limit for vacancy id."""
-        self._max_age = db.get_expired_time()
-
-    def _create_db(self) -> None:
-        """Creating DB."""
-        db.create_db()
-
-    def _db_is_exists(self) -> bool:
-        """Check for db is already exist."""
-        if not self._db_file:
-            self._set_db_filename()
-
-        return db.file_is_exists(self._db_file)
-
-    def _sift_vacancies(self, vacancies_id: list) -> list:
-        """Sift vacancies to leave new ones. New vacancies save in database."""
-        old_vacancies: set = self.load()
-        new_vacancies: list = [
-            vid for vid in vacancies_id if vid not in old_vacancies
-        ]
-        self.save(new_vacancies)
-        return new_vacancies
-
-    async def _async_get_response_data(
+    async def _get_response_data(
         self,
         urls: list,
         delay: float | int
@@ -98,6 +49,15 @@ class BaseVacancyCollector:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 return await response.json()
+
+    def _sift_vacancies(self, vacancies_id: list) -> list:
+        """Sift vacancies to leave new ones. New vacancies save in database."""
+        old_vacancies: set = self._storage.load()
+        new_vacancies: list = [
+            vid for vid in vacancies_id if vid not in old_vacancies
+        ]
+        self._storage.save(vacancies_id)
+        return new_vacancies
 
     def make_request_url_with_params(
         self,
@@ -129,32 +89,3 @@ class BaseVacancyCollector:
         ]
 
         return request_url + '&'.join(params_string)
-
-    def save(self, vacancies_id: list) -> None:
-        """Save vacancy id in db with timestamp."""
-        with shelve.open(self._db_file) as vdb:
-            for vid in vacancies_id:
-                if vid not in vdb:
-                    current_time = dt.datetime.now()
-                    vdb[vid] = dt.datetime.timestamp(current_time)
-
-    def load(self) -> set:
-        """Loading set of vacancies id. Remove id if expired."""
-        vacancies_id: set = {}
-        time_delta = dt.timedelta(self._max_age)
-        expired_ids: list = []
-
-        with shelve.open(self._db_file) as vdb:
-            vacancies_id = set(vdb.keys())
-
-            for vid in vdb:
-                pub_date = dt.datetime.fromtimestamp(vdb[vid])
-                current_date = dt.datetime.now()
-                if pub_date + time_delta < current_date:
-                    expired_ids.append(vid)
-
-            if expired_ids:
-                for vid in expired_ids:
-                    vdb.pop(vid)
-
-        return vacancies_id
