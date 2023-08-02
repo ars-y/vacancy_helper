17eacd30a64c8b78a0b9b73a0585a2470be4e0eb
ArslanYadov <arsyy90@gmail.com>
2023-08-02 12:16:09 +0300

    refactoring: new class VIDStorage to manage database operations

--- db.py
+++ db.py
@@ -1,36 +1,66 @@
+import datetime as dt
 import os
 import shelve
 
 from constants import DB_DIR, DB_NAME, MAX_AGE
 
 
-def dir_is_exists(db_path: str) -> bool:
-    """Check db catalog is exists"""
-    return os.path.isdir(db_path)
+class VIDStorage:
+    """Vacancies ID database."""
 
+    def __init__(self) -> None:
+        self._db_path = DB_DIR
+        self._db_name = DB_NAME
+        self._max_age = MAX_AGE
 
-def file_is_exists(db_path: str) -> bool:
-    """Check db file is exists."""
-    return os.path.exists(db_path)
+        self._prepare_database()
 
+    def _create_db(self) -> None:
+        """Create db dir and file in that dir."""
+        if not self._dir_is_exists():
+            os.mkdir(self._db_path)
 
-def create_db() -> None:
-    """Create db dir and file in that dir."""
-    db_path = DB_DIR
-    filename: str = DB_NAME
+        with shelve.open(self._db_name):
+            pass
 
-    if not dir_is_exists(db_path):
-        os.mkdir(db_path)
+    def _dir_is_exists(self) -> bool:
+        """Check db catalog is exists"""
+        return os.path.isdir(self._db_path)
 
-    with shelve.open(filename):
-        pass
+    def _file_is_exists(self) -> bool:
+        """Check db file is exists."""
+        return os.path.exists(self._db_path)
 
+    def _prepare_database(self) -> None:
+        """Creating database if it isn't exists."""
+        if not self._file_is_exists():
+            self._create_db()
 
-def get_db_filename() -> str:
-    """Return db filename"""
-    return DB_NAME
+    def save(self, vacancies_id: list) -> None:
+        """Save vacancies id in db with timestamp."""
+        with shelve.open(self._db_name) as vdb:
+            for vid in vacancies_id:
+                if vid not in vdb:
+                    current_time = dt.datetime.now()
+                    vdb[vid] = dt.datetime.timestamp(current_time)
 
+    def load(self) -> set:
+        """Loading set of vacancies id. Remove id if expired."""
+        vacancies_id: set = {}
+        time_delta = dt.timedelta(self._max_age)
+        expired_ids: list = []
 
-def get_expired_time() -> int:
-    """Return expired time for id"""
-    return MAX_AGE
+        with shelve.open(self._db_name) as vdb:
+            vacancies_id = set(vdb.keys())
+
+            for vid in vdb:
+                pub_date = dt.datetime.fromtimestamp(vdb[vid])
+                current_date = dt.datetime.now()
+                if pub_date + time_delta < current_date:
+                    expired_ids.append(vid)
+
+            if expired_ids:
+                for vid in expired_ids:
+                    vdb.pop(vid)
+
+        return vacancies_id
