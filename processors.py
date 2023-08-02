7a3df29669cc15af6f9cb9f11ee54f391f3b6574
ArslanYadov <arsyy90@gmail.com>
2023-08-02 11:30:04 +0300

    refactoring: collect text processors utils in class TextProcessor

--- processors.py
+++ processors.py
@@ -1,53 +1,57 @@
 import re
 
-
-TAG_PATTERN: re.Pattern = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
-
-URL_PATTERN: re.Pattern = re.compile(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))")
+from constants import TAG_PATTERN, URL_PATTERN
 
 
-def remove_tags_from_text(string: str) -> list:
-    """Remove html tags from string."""
-    return re.sub(TAG_PATTERN, '', string)
+TAG_PATTERN: re.Pattern = re.compile(TAG_PATTERN)
+URL_PATTERN: re.Pattern = re.compile(URL_PATTERN)
 
 
-def add_carry(strings: list) -> None:
-    """
-    Replace ':' and ';' with hypen bullet.
-    Add extra carry after dot.
-    """
-    for i in range(len(strings) - 1):
-        if ':' in strings[i] or ';' in strings[i]:
-            strings[i] += '\n-'
-        elif '.' in strings[i]:
-            strings[i] += '\n\n'
+class TextProcessor:
+    """Set of text processing methods."""
 
+    def __init__(self) -> None:
+        pass
 
-def remove_empty_elements(strings: list) -> list:
-    return [string for string in strings if string != '']
+    def cleaning_data(self, strings: str) -> str:
+        """
+        Cleaning html tags from input strings if exists.
+        Additing bullet and carry characters.
+        """
+        if not strings:
+            return
 
+        strings: list = strings.split()
 
-def process_text(strings: str) -> str:
-    """
-    Cleaning html tags from input strings if exists.
-    Additing bullet and carry characters.
-    """
-    if not strings:
-        return
+        for i in range(len(strings)):
+            _match: re.Match | None = re.search(TAG_PATTERN, strings[i])
+            if _match:
+                strings[i] = self._remove_tags(strings[i])
 
-    strings: list = strings.split()
+        strings: list = self._remove_empty_elements(strings)
+        self._add_carry(strings)
 
-    for i in range(len(strings)):
-        _match: re.Match | None = re.search(TAG_PATTERN, strings[i])
-        if _match:
-            strings[i] = remove_tags_from_text(strings[i])
+        return ' '.join(strings)
 
-    strings: list = remove_empty_elements(strings)
-    add_carry(strings)
+    def _add_carry(self, strings: list) -> None:
+        """
+        Replace ':' and ';' with hypen bullet.
+        Add extra carry after dot.
+        """
+        for i in range(len(strings) - 1):
+            if ':' in strings[i] or ';' in strings[i]:
+                strings[i] += '\n-'
+            elif '.' in strings[i]:
+                strings[i] += '\n\n'
 
-    return ' '.join(strings)
+    def _remove_tags(self, string: str) -> list:
+        """Remove html tags from string."""
+        return re.sub(TAG_PATTERN, '', string)
 
+    def _remove_empty_elements(self, strings: list) -> list:
+        return [string for string in strings if string != '']
 
-def is_correct_url(url: str) -> bool:
-    """Check url is correct."""
-    return re.match(URL_PATTERN, url) is not None
+    @staticmethod
+    def is_correct_url(url: str) -> bool:
+        """Check url is correct."""
+        return re.match(URL_PATTERN, url) is not None
