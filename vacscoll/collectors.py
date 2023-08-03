70bfbba5d3eb09090dc37cf85af3e55afb6e37f1
ArslanYadov <arsyy90@gmail.com>
2023-08-03 20:48:54 +0300

    all sync request to web api is async already

--- vacscoll/collectors.py
+++ vacscoll/collectors.py
@@ -1,5 +1,4 @@
 import asyncio
-import requests
 
 from .bases import BaseVacancyCollector
 from .constants import HH_REQUEST_DELAY
@@ -33,41 +32,37 @@ class VacancyHHCollector(BaseVacancyCollector):
 
         return processed_items
 
-    def _extract_rest_vacancies(self, url: str, total_pages: int) -> list:
+    async def _extract_rest_vacancies(
+        self,
+        url: str,
+        total_pages: int
+    ) -> list:
         """Extract vacancies id from rest pages."""
         urls: list = [
             url + '&page=%d' % page_num
             for page_num in range(1, total_pages)
         ]
 
-        dataset: list = []
-
-        try:
-            loop = asyncio.get_running_loop()
-        except RuntimeError:
-            loop = None
-        
-        if loop and loop.is_running():
-            dataset = self._sync_get_response_data(urls)
-        else:
-            dataset = asyncio.run(
-                self._get_response_data(urls, self._delay)
-            )
+        dataset = await self._get_response_data(urls, self._delay)
 
         return [
             item for data in dataset
             for item in data.get('items')
         ]
 
-    def _get_vacancies_id(self, url: str) -> list:
+    async def _get_vacancies_id(self, url: str) -> list:
         """
         Collecting vacancies id in list
         from all vacancies with specified parameters.
         If response contains more than one page,
         then make async requests for remaining pages.
         """
-        response: requests.Response = requests.get(url)
-        data = response.json()
+
+        dataset: list = await asyncio.gather(
+            asyncio.create_task(self._make_request(url))
+        )
+
+        data: dict = dict(*dataset)
 
         vacancies_items: list = data.get('items')
 
@@ -75,9 +70,12 @@ class VacancyHHCollector(BaseVacancyCollector):
         total_pages: int = data.get('pages', 0)
 
         if current_page < total_pages:
-            vacancies_items.extend(
-                self._extract_rest_vacancies(url, total_pages)
+            items: list = await asyncio.gather(
+                asyncio.create_task(
+                    self._extract_rest_vacancies(url, total_pages)
+                )
             )
+            vacancies_items.extend(*items)
 
         if self._filters:
             vacancies_items: list = self._apply_filters(vacancies_items)
@@ -86,7 +84,7 @@ class VacancyHHCollector(BaseVacancyCollector):
 
         return vacancies_id
 
-    def recieve_vacancies(self, vacancies_id: list) -> list:
+    async def recieve_vacancies(self, vacancies_id: list) -> list:
         """
         Recieve Vacancy ojects.
         Method consruct requests for async get response data
@@ -99,14 +97,14 @@ class VacancyHHCollector(BaseVacancyCollector):
             for vid in vacancies_id
         ]
 
-        dataset: list = asyncio.run(
-            self._get_response_data(request_urls, self._delay)
+        dataset: list = await self._get_response_data(
+            request_urls, self._delay
         )
 
         return [VacancyHH(item) for item in dataset]
 
-    def run(self) -> list:
+    async def run(self) -> list:
         """Collecting vacancies and return sift vacancies id."""
         request_url: str = self.make_request_url_with_params()
-        vacancies_id: list = self._get_vacancies_id(request_url)
+        vacancies_id: list = await self._get_vacancies_id(request_url)
         return self._sift_vacancies(vacancies_id)
