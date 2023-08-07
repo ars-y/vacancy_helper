d3817d6a644d8f223b2728118a165dc01f6fb7d6
ArslanYadov <arsyy90@gmail.com>
2023-08-07 22:16:56 +0300

    add getting vacancies id worker

---
+++ vacscoll/workers.py
@@ -0,0 +1,17 @@
+from .collectors import VacancyHHCollector
+
+
+async def get_vacsid(src_name: str, keywords: str) -> list:
+    """Getting vacancies id list form job aggregators API."""
+    if src_name == 'hh':
+        text_param: str = '+'.join(keywords.split())
+        params: dict = {
+            'text': text_param,
+            'per_page': '100',
+            'no_magic': 'true',
+        }
+        url: str = 'https://api.hh.ru'
+        endpoint: str = 'vacancies'
+
+        vacscoll = VacancyHHCollector(url, endpoint, params)
+        return await vacscoll.run()
