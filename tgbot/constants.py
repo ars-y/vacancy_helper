a6226004996e2c1efe30920692ec79fa018a4bd5
ArslanYadov <arsyy90@gmail.com>
2023-08-08 19:00:54 +0300

    add more constants for callback buttons; chunk size var for vacancy chunk in output

--- tgbot/constants.py
+++ tgbot/constants.py
@@ -1,7 +1,11 @@
-END_ROUTES, START_ROUTES = range(2)
+END_ROUTES, START_ROUTES, SEND_VACS = range(3)
 
 HH_BUTTON: str = 'hh'
 
 ALL_BUTTON: str = 'all'
 
 BACK_BUTTON: str = 'back'
+
+NEXT_BUTTON: str = 'next'
+
+CHUNK_SIZE: int = 3
