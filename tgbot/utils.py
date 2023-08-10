37d3fdb743bbddd513896917cd65811363c584d5
ArslanYadov <arsyy90@gmail.com>
2023-08-10 11:50:16 +0300

    message is consructed from list instead of creating new strings

--- tgbot/utils.py
+++ tgbot/utils.py
@@ -21,16 +21,16 @@ def check_tokens(*args) -> None:
 
 def format_message(vacancy: VacancyHH) -> str:
     """Construct message with info about vacancy."""
-    vacancy_info: str = (
-        f'{vacancy.name}\n'
-        f'{vacancy.employment}\n'
-        f'Компания: {vacancy.employer}\n\n'
-    )
+    vacancy_info: list = [
+        f'{vacancy.name}\n',
+        f'{vacancy.employment}\n',
+        f'Компания: {vacancy.employer}\n\n',
+    ]
 
     if vacancy.requirements:
-        vacancy_info += f'Требования:\n{vacancy.requirements}\n\n'
+        vacancy_info.append(f'Требования:\n{vacancy.requirements}\n\n')
 
     if vacancy.responsibility:
-        vacancy_info += f'Обязанности:\n{vacancy.responsibility}\n'
+        vacancy_info.append(f'Обязанности:\n{vacancy.responsibility}\n')
 
-    return vacancy_info
+    return ''.join(vacancy_info)
