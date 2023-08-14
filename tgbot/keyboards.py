2e6e868492ad6542cfef4fdc94df6482c1dc17dd
ArslanYadov <arsyy90@gmail.com>
2023-08-14 21:06:31 +0300

    refactoring: single keyboard factory instead of sets

--- tgbot/keyboards.py
+++ tgbot/keyboards.py
@@ -1,36 +1,22 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from .constants import HH_BUTTON, NEXT_BUTTON, SKIP_BUTTON, BACK_BUTTON
 
-
-def select_keyboard() -> InlineKeyboardMarkup:
-    """Keyboard to choosing vacancy aggregator."""
-    return InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton('Подбор с hh.ru', callback_data=HH_BUTTON)],
-        ]
-    )
-
-
-def move_to_keyboard(text: str, callback_data: str) -> InlineKeyboardMarkup:
-    """Make one button with text and callback data."""
-    return InlineKeyboardMarkup(
-        [[InlineKeyboardButton(text, callback_data=callback_data)]]
-    )
-
-
-def url_keyboard(url: str) -> InlineKeyboardMarkup:
+def url_keyboard(text: str, url: str) -> InlineKeyboardMarkup:
     """Make url button under vacancy info."""
     return InlineKeyboardMarkup(
-        [[InlineKeyboardButton('Подробнее', url=url)]]
+        [[InlineKeyboardButton(text, url=url)]]
     )
 
 
-def next_skip_back(_next: str, _skip: str, _back: str) -> InlineKeyboardMarkup:
+def build_keyboard(button_rows: list[tuple]) -> InlineKeyboardMarkup:
+    """
+    One column keyboard constructor.
+    Args:
+        list of tuple: (label text, callback_data)
+    """
     return InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton(_next, callback_data=NEXT_BUTTON)],
-            [InlineKeyboardButton(_skip, callback_data=SKIP_BUTTON)],
-            [InlineKeyboardButton(_back, callback_data=BACK_BUTTON)],
+            [InlineKeyboardButton(text, callback_data=callback_data)]
+            for text, callback_data in button_rows
         ]
     )
